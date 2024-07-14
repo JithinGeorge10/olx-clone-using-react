0862679f2195580e4a0fcafc075a0d7685d90da3
JithinGeorge10 <jithin.george.el@gmail.com>
2024-07-14 15:18:47 +0530

    Home page

---
+++ olx-clone/src/Components/Posts/Posts.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import Heart from '../../assets/Heart';
+import './Posts.css';
+
+function Posts() {
+
+  return (
+    <div className="postParentDiv">
+      <div className="moreView">
+        <div className="heading">
+          <span>Quick Menu</span>
+          <span>View more</span>
+        </div>
+        <div className="cards">
+          <div
+            className="card"
+          >
+            <div className="favorite">
+              <Heart></Heart>
+            </div>
+            <div className="image">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyWcQK9ds-Fv_O-fiz0I1twsSdiDEFYsUWLA&s" alt="" />
+            </div>
+            <div className="content">
+              <p className="rate">&#x20B9; 250000</p>
+              <span className="kilometer">Two Wheeler</span>
+              <p className="name"> YAMAHA R15V3</p>
+            </div>
+            <div className="date">
+              <span>Tue May 04 2021</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="recommendations">
+        <div className="heading">
+          <span>Fresh recommendations</span>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <div className="favorite">
+              <Heart></Heart>
+            </div>
+            <div className="image">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyWcQK9ds-Fv_O-fiz0I1twsSdiDEFYsUWLA&s" alt="" />
+            </div>
+            <div className="content">
+              <p className="rate">&#x20B9; 250000</p>
+              <span className="kilometer">Two Wheeler</span>
+              <p className="name"> YAMAHA R15V3</p>
+            </div>
+            <div className="date">
+              <span>10/5/2021</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Posts;
--- olx-clone/src/Pages/Home.jsx
+++ olx-clone/src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../Components/Header/Header.jsx'
 import Banner from '../Components/Banner/Banner.jsx'
+import Posts from '../Components/Posts/Posts.jsx'
+import Footer from '../Components/Footer/Footer.jsx'
 function Home() {
     return (
         <div>
             <Header />
-            <Banner/>
+            <Banner />
+            <Posts />
+            <Footer/>
         </div>
     )
 }
